0df45f04ae426d16898ad7649979f94d06348558
Ann Fisher <annf@middlebury.edu>
2022-12-09 13:24:08 -0800

    Testing

--- Top-Down-Car-Racing-game-in-windows-form-main/Car Racing Game MOO ICT/Form1.Designer.cs
+++ Top-Down-Car-Racing-game-in-windows-form-main/Car Racing Game MOO ICT/Form1.Designer.cs
@@ -29,30 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnStart = new System.Windows.Forms.Button();
             this.txtScore = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.roadTrack1 = new System.Windows.Forms.PictureBox();
-            this.roadTrack2 = new System.Windows.Forms.PictureBox();
-            this.player = new System.Windows.Forms.PictureBox();
-            this.AI1 = new System.Windows.Forms.PictureBox();
-            this.AI2 = new System.Windows.Forms.PictureBox();
-            this.explosion = new System.Windows.Forms.PictureBox();
-            this.award = new System.Windows.Forms.PictureBox();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AI2 = new System.Windows.Forms.PictureBox();
+            this.AI1 = new System.Windows.Forms.PictureBox();
+            this.award = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.roadTrack2 = new System.Windows.Forms.PictureBox();
+            this.roadTrack1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AI1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.AI2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.award)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            resources.ApplyResources(this.panel1, "panel1");
             this.panel1.BackColor = System.Drawing.Color.Black;
             this.panel1.Controls.Add(this.AI2);
             this.panel1.Controls.Add(this.AI1);
@@ -61,143 +65,120 @@
             this.panel1.Controls.Add(this.player);
             this.panel1.Controls.Add(this.roadTrack2);
             this.panel1.Controls.Add(this.roadTrack1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(475, 519);
-            this.panel1.TabIndex = 0;
             // 
             // btnStart
             // 
-            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStart.Location = new System.Drawing.Point(187, 590);
+            resources.ApplyResources(this.btnStart, "btnStart");
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(99, 41);
-            this.btnStart.TabIndex = 1;
-            this.btnStart.Text = "Start";
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.restartGame);
             // 
             // txtScore
             // 
-            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtScore.Location = new System.Drawing.Point(12, 534);
+            resources.ApplyResources(this.txtScore, "txtScore");
             this.txtScore.Name = "txtScore";
-            this.txtScore.Size = new System.Drawing.Size(475, 37);
-            this.txtScore.TabIndex = 2;
-            this.txtScore.Text = "Score: 0";
-            this.txtScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 634);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(475, 138);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Press Left and Right to move the car. \r\n\r\nDon\'t hit any other cars in the game an" +
-    "d survive as long as you can";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // roadTrack1
-            // 
-            this.roadTrack1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
-            this.roadTrack1.Location = new System.Drawing.Point(0, -519);
-            this.roadTrack1.Name = "roadTrack1";
-            this.roadTrack1.Size = new System.Drawing.Size(475, 519);
-            this.roadTrack1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.roadTrack1.TabIndex = 0;
-            this.roadTrack1.TabStop = false;
-            // 
-            // roadTrack2
-            // 
-            this.roadTrack2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
-            this.roadTrack2.Location = new System.Drawing.Point(0, 0);
-            this.roadTrack2.Name = "roadTrack2";
-            this.roadTrack2.Size = new System.Drawing.Size(475, 519);
-            this.roadTrack2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.roadTrack2.TabIndex = 4;
-            this.roadTrack2.TabStop = false;
-            // 
-            // player
-            // 
-            this.player.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carYellow;
-            this.player.Location = new System.Drawing.Point(215, 402);
-            this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(50, 99);
-            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.player.TabIndex = 5;
-            this.player.TabStop = false;
-            // 
-            // AI1
-            // 
-            this.AI1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGreen;
-            this.AI1.Location = new System.Drawing.Point(76, 62);
-            this.AI1.Name = "AI1";
-            this.AI1.Size = new System.Drawing.Size(50, 101);
-            this.AI1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.AI1.TabIndex = 5;
-            this.AI1.TabStop = false;
-            this.AI1.Tag = "carLeft";
-            // 
-            // AI2
-            // 
-            this.AI2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGrey;
-            this.AI2.Location = new System.Drawing.Point(422, 62);
-            this.AI2.Name = "AI2";
-            this.AI2.Size = new System.Drawing.Size(50, 100);
-            this.AI2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.AI2.TabIndex = 5;
-            this.AI2.TabStop = false;
-            this.AI2.Tag = "carRight";
-            // 
-            // explosion
-            // 
-            this.explosion.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.explosion;
-            this.explosion.Location = new System.Drawing.Point(76, 375);
-            this.explosion.Name = "explosion";
-            this.explosion.Size = new System.Drawing.Size(64, 64);
-            this.explosion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.explosion.TabIndex = 5;
-            this.explosion.TabStop = false;
-            // 
-            // award
-            // 
-            this.award.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.bronze;
-            this.award.Location = new System.Drawing.Point(116, 207);
-            this.award.Name = "award";
-            this.award.Size = new System.Drawing.Size(250, 100);
-            this.award.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.award.TabIndex = 5;
-            this.award.TabStop = false;
             // 
             // gameTimer
             // 
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimerEvent);
             // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2")});
+            this.comboBox1.Name = "comboBox1";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // AI2
+            // 
+            resources.ApplyResources(this.AI2, "AI2");
+            this.AI2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGrey;
+            this.AI2.Name = "AI2";
+            this.AI2.TabStop = false;
+            this.AI2.Tag = "carRight";
+            // 
+            // AI1
+            // 
+            resources.ApplyResources(this.AI1, "AI1");
+            this.AI1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carGreen;
+            this.AI1.Name = "AI1";
+            this.AI1.TabStop = false;
+            this.AI1.Tag = "carLeft";
+            // 
+            // award
+            // 
+            resources.ApplyResources(this.award, "award");
+            this.award.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.bronze;
+            this.award.Name = "award";
+            this.award.TabStop = false;
+            // 
+            // explosion
+            // 
+            resources.ApplyResources(this.explosion, "explosion");
+            this.explosion.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.explosion;
+            this.explosion.Name = "explosion";
+            this.explosion.TabStop = false;
+            // 
+            // player
+            // 
+            resources.ApplyResources(this.player, "player");
+            this.player.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.carYellow;
+            this.player.Name = "player";
+            this.player.TabStop = false;
+            // 
+            // roadTrack2
+            // 
+            resources.ApplyResources(this.roadTrack2, "roadTrack2");
+            this.roadTrack2.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
+            this.roadTrack2.Name = "roadTrack2";
+            this.roadTrack2.TabStop = false;
+            // 
+            // roadTrack1
+            // 
+            resources.ApplyResources(this.roadTrack1, "roadTrack1");
+            this.roadTrack1.Image = global::Car_Racing_Game_MOO_ICT.Properties.Resources.roadTrack;
+            this.roadTrack1.Name = "roadTrack1";
+            this.roadTrack1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(499, 781);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtScore);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Car Racing Game MOO ICT";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AI1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.AI2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.award)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.roadTrack1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -215,6 +196,8 @@
         private System.Windows.Forms.PictureBox explosion;
         private System.Windows.Forms.PictureBox player;
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
